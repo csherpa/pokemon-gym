ea6376c12e2f0b1a309d4ed927090aaf255a84a0
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-21 08:51:20 -0400

    test 1

---
+++ kevin-script.js
@@ -0,0 +1,38 @@
+const setPokemonCarousel = id => {
+    const carousel = document.getElementById(id),
+          figure = carousel.querySelector('figure'),
+          nav = carousel.querySelector('nav'),
+          num = figure.childElementCount,
+          theta = 2 * Math.PI / num;
+          // 2*PI*r = circumference of circle
+          // theta = degree made btwn two apothems
+
+    let curr = 0;
+
+    const onClick = e => {
+        e.stopPropagation();
+
+        const t = e.target;
+        if (t.tagName.toUpperCase() != 'I')
+            return;
+
+        if (t.classList.contains('fa-angle-right')) {
+            curr++;
+        } else {
+            curr--;
+        }
+
+        // 1 rad = 180deg
+        figure.style.transform = `rotateY(${curr * -theta}rad)`;
+    }
+
+    nav.addEventListener('click', onClick, true);
+}
+
+for (let i = 1; i < 4; i++) {
+    setPokemonCarousel(`pokemon-carousel-${i}`);
+}
+
+const setTrainerCarousel = id => {
+    
+}
