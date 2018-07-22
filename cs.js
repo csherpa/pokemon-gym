014feb342a7473fb347133b6192e3ee3d1bf9b9b
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-21 23:40:45 -0400

    added template strings, removed a lot of hard coded html

--- cs.js
+++ cs.js
@@ -1,109 +1,119 @@
-// class Gym{
-//     constructor(){
-//         this.allTrainers = [];
-//     }
-//     add(trainerObj){
-//         this.allTrainers.push(trainerObj);
-//     }
-// }
+class Gym {
+    constructor () {
+        this.allTrainers = [];
+    }
+    add (trainer) {
+        this.allTrainers.push(trainer);
+    }
+    get (trainerName) {
+       for (let i of this.allTrainers) {
+         if (i.name === trainerName) return i;
+       }
+    }
+    all () {
+        return this.allTrainers;
+    }
+}
 
-// let gymMembers = new Gym();
+const gymMembers = new Gym();
 
-// class Trainers{
-//     constructor(){
-//         this.allPokemon = [];
-//     }   
+class Trainers {
+    constructor(name) {
+        this.name = name;
+        this.allPokemon = [];
+    }
 
-//     add(pokeObj){
-//         this.allPokemon.push(pokeObj);
-//     }
-//     get(name){
-//         return this.allPokemon.find((element) => {
-//             return element === name;
-//         })
+    add(pokeObj) {
+        this.allPokemon.push(pokeObj);
+    }
+    get(name) {
+        return this.allPokemon.find((element) => {
+            return element === name;
+        })
+    }
+    all() {
+        return this.allPokemon;
+    }
 
-//     }
+}
+
+class Pokemon {
+    constructor(name, hp, attack, defense, abilities, photo) {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.abilities = abilities;
+        this.photo = photo;
+    }
+}
+
+// global var to track how many trainers have been added so far to DOM
+let trainerNum = 0;
+
+// function that will assign content
+const assignContent = (trainersPokemon, pokemonNum) => {
+    const trainerName = gymMembers.all()[trainerNum].name;
+    document.getElementById(trainerName).textContent = trainerName.toUpperCase();
+
+    const target = document.getElementById(`${trainerName}-${pokemonNum}`);
+    console.log(target);
+    const template =
+        `<img class='images' src='${trainersPokemon.photo}'></img>
+        <h3>${trainersPokemon.name}</h3>
+        <p>HP: ${trainersPokemon.hp}</p>
+        <p>Attack: ${trainersPokemon.attack}</p>
+        <p>Defense: ${trainersPokemon.defense}</p>
+        <p>Abilities ${trainersPokemon.abilities.join(', ')}</p>`
+
+    const div = document.createElement('div');
+    div.innerHTML = template;
     
-// }
+    target.appendChild(div);
+}
 
-// let chhoti = new Trainers();
-// let kevin = new Trainers();
-// let jihae = new Trainers();
+let pokemonSearch = (idValue, trainer) => {
+  // Function will:
+  // 1. add trainer to gymMembers
+  // 2. add pokemon to trainer
+  // 3. if trainer has 3 pokemon, it will render info to the DOM
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${idValue}`).then((response) => {
+        const data = response.data;
 
-// class Pokemon{
-//     constructor(name, hp, attack, defense, abilities){
-//         this.name = name;
-//         this.hp = hp;
-//         this.attack = attack;
-//         this.defense = defense;
-//         this.abilities = abilities;
-//     }
+        const pokemon = new Pokemon(
+            data.name,
+            data.stats[5].base_stat,
+            data.stats[4].base_stat,
+            data.stats[3].base_stat,
+            data.abilities.map(x => x.ability.name),
+            data.sprites.front_default
+        )
 
-// }
+        const trainerObj = new Trainers(trainer);
+        if (!gymMembers.get(trainer)) gymMembers.add(trainerObj);
+        gymMembers.get(trainer).add(pokemon);
+    }).then(() => {
+        const trainersPokemon = gymMembers.get(trainer).all();
+        if (trainersPokemon.length === 3) {
+            for (let i = 0; i < 3; i++) {
+                assignContent(trainersPokemon[i], i+1);
+            }
+            trainerNum++;
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+}
 
-// let pokemonSearch = (idValue,trainer) =>{
-//     axios.get(`https://pokeapi.co/api/v2/pokemon/${idValue}`).then((response)=>{
-//     let data = response.data;
-//     console.log(data);
+// Add starter pokemon
+const starterPokemon = {
+    kevin: [59,130,149],
+    jihae: [50,200,238],
+    chhoti: [260,286,802]
+};
 
-//     let getAbilities = [];
-//     data.abilities.forEach((element)=>{
-//         getAbilities.push(element.ability.name);
-//     });
-//     console.log(getAbilities);
-
-//     let pokemon = new Pokemon(
-//         data['name'],
-//         data.stats[5].base_stat,
-//         data.stats[4].base_stat,
-//         data.stats[3].base_stat,
-//         getAbilities
-//     )
-
-//     let stat = data.stats;
-//     console.log(stat);
-
-//     let hp = stat[5].base_stat;
-//     let para1 = document.createElement('p');
-//     para1.textContent = 'HP : ' + hp;
-//     document.body.appendChild(para1);
-//     console.log(hp);
-    
-
-//     let attack = stat[4].base_stat;
-//     let para2 = document.createElement('p');
-//     para2.textContent = 'Attack : ' + attack;
-//     document.body.appendChild(para2);
-//     console.log(attack);
-
-//     let defense = stat[3].base_stat;
-//     let para3 = document.createElement('p');
-//     para3.textContent = 'Defense : ' + defense;
-//     document.body.appendChild(para3);
-//     console.log(defense);
-
-//     let para = document.createElement('p');
-//     para.innerHTML = 'Ability : ' + getAbilities.join(", ");
-//     document.body.appendChild(para);
-    
-//     trainer.add(pokemon);
-
-//     gymMembers.add(trainer);
-    
-
-// }).catch((error) =>{
-//     console.log(error);
-// })
-// }
-
-// const starterPokemon = {
-//     kevin: [59,130,149],
-//     chhoti: [260,286,802],
-//     jihae: [50,200,238]
-// };
-
-// for (let k in starterPokemon) {
-//     for (let v of starterPokemon[k]) {
-//         pokemonSearch(v, k);
-//     }
-// };
+for (let k in starterPokemon) {
+    for (let v of starterPokemon[k]) {
+        pokemonSearch(v, k);
+    }
+}
